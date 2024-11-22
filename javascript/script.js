1fa7d1444507ca33775939a2789b6ee924d3bb81
kevin12332111 <mamanikevin327@gmail.com>
2024-11-21 22:41:51 -0400

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// script.js
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Datos de usuario predefinidos (puedes cambiarlos)
+    const validUsername = "kevin";
+    const validPassword = "73852622";
+
+    if (username === validUsername && password === validPassword) {
+        alert("Inicio de sesión exitoso!");
+        // Aquí puedes redirigir al usuario a otra página o realizar otras acciones
+        window.location.href = "dashboard.html"; // Ejemplo de redirección
+    } else {
+        document.getElementById("error-message").textContent = "Usuario o contraseña incorrectos.";
+    }
+});
